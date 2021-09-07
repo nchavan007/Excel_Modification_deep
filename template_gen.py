a30461cdda173326f45281ffac780274ba3ea12a
nchavan007 <50837588+nchavan007@users.noreply.github.com>
2021-09-07 04:58:25 -0700

    New changes

--- template_gen.py
+++ template_gen.py
@@ -21,18 +21,18 @@ tuple_list = [('B',1), ('C',2), ( 'D', 3)]
 
 for row in main_sheet.iter_rows(min_row=2, values_only=True):
    for i, j in tuple_list:
-       for k in temp1_sheet:
-           template1[k][f'{i}2'] = row[j]
-
-       for k in temp2_sheet:
-           template2[k][f'{i}2'] = row[j]
-
-       for k in temp3_sheet:
-           template3[k][f'{i}2'] = row[j]
-
-       for k in temp4_sheet:
-           template4[k][f'{i}2'] = row[j]
-
+       if row[17] == "A":
+           for k in temp1_sheet:
+               template1[k][f'{i}2'] = row[j]
+       if row[17] == "B":
+           for k in temp2_sheet:
+               template2[k][f'{i}2'] = row[j]
+       if row[17] == "C":
+           for k in temp3_sheet:
+               template3[k][f'{i}2'] = row[j]
+       if row[17] == "D":
+           for k in temp4_sheet:
+               template4[k][f'{i}2'] = row[j]
    template1.save(filename=f'template1_{row[0]}.xlsx')
    template2.save(filename=f'template2_{row[0]}.xlsx')
    template3.save(filename=f'template3_{row[0]}.xlsx')
